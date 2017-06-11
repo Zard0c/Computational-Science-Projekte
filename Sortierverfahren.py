fdc7d7b7a59daaee658df18ac7515a16c1901647
Zard0c <max_dow@hotmail.de>
2017-06-11 21:14:48 +0200

    Create Sortierverfahren.py

--- Sortierverfahren.py
+++ Sortierverfahren.py
@@ -23,17 +23,17 @@ def sortMerge(a):
 
 def merge(a, b):
     result = []
-    while (len(a) != 0) and (len(b) != 0): #Zusammenführen bis eine Liste leer
-        if a[0] < b[0]: #Anhängen des kleineren der beiden Elemente an Ergebnis
+    while (len(a) != 0) and (len(b) != 0): #Zusammenführen bis eine Liste leer ist
+        if a[0] < b[0]: #Anhängen des kleineren der beiden Elemente an das Ergebnis
             result.append(a[0])
             a = a[1:len(a)] #Erstes Element von a entfernen
         else:
             result.append(b[0])
             b = b[1:len(b)] #Erstes Element von b entfernen
-    if a != []: #Falls a noch nicht leer
-        result = result + a #Anhängen von a an Ergebnis
-    if b != []: #Falls b noch nicht leer
-        result = result + b #Anhängen von b an Ergebnis
+    if a != []: #Falls a noch nicht leer ist
+        result = result + a #Anhängen von a an das Ergebnis
+    if b != []: #Falls b noch nicht leer ist
+        result = result + b #Anhängen von b an das Ergebnis
     return result
 
 def sortHeapDesc(a):
@@ -52,15 +52,15 @@ def sortHeapDesc(a):
 
     while l > 0:
         a = swap(a, 0, l)
-        print("Sortierschirtt: ", a)
-        l -= 1 #Leztztes Element gehört nicht mehr zum Baum
+        print("Sortierschritt: ", a)
+        l -= 1 #Letztes Element gehört nicht mehr zum Baum
         a = heapify(a, 0, l) #Heapeigenschaft wieder herstellen
         print("Neuer Heap: ", a)
 
     return a
 
 def heapify(a, p, l): #p: Knoten für den Heapeigenschaft hergestellt wird, l: Ende des Baums
-    if 2*p+1 > l: #p hat kein Kind, daher nichts zu machen
+    if 2*p+1 > l: #p hat kein Kind, daher ist nichts zu machen
         return a
     if 2*p+1 == l: #p hat nur ein Kind
         if a[p] < a[2*p+1]:
@@ -69,7 +69,7 @@ def heapify(a, p, l): #p: Knoten für den Heapeigenschaft hergestellt wird, l: E
             a = swap(a, p, 2*p+1)
             return a
     if a[2*p+1] < a[2*p+2]: #Bestimmen des kleineren Kindes von p
-        if a[2*p+1] < a[p]: #Wenn Kind kleiner als Vater
+        if a[2*p+1] < a[p]: #Wenn Kind kleiner als Vater ist
             a = swap(a, p, 2*p+1) #Tauschen
             a = heapify(a, 2*p+1, l) #Heapeigenschaft für verändertes Kind wiederherstellen
     elif a[2*p+2] < a[p]:
